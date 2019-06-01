dcef7380213b4441932a1a4149cb559ec110311f
chetta chetta chetta <>
2019-06-01 21:43:34 +0700

    first commit

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chettachettachetta/Documents/Labs/Python/Site #3 - Product Hunt Clone/myvenv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
